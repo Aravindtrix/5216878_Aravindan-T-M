98c32828956b431a516854073c1bed498b355a49
AravindTrix <aravindtrix@gmail.com>
2025-07-27 01:53:09 +0530

    add reverse number in feature2

--- Git/practice.c
+++ Git/practice.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 
@@ -33,4 +34,12 @@ int main() {
     return 0;
 }
 
+int reverse_number(int num) {
+    int reversed = 0;
+    while (num != 0) {
+        reversed = reversed * 10 + (num % 10);
+        num /= 10;
+    }
+    return reversed;
+}
 
