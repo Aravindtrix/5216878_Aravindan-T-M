3028ed34c1903accb1f05634399f1111bd073671
AravindTrix <aravindtrix@gmail.com>
2025-07-27 01:46:23 +0530

    add practice.c with factorial logic

---
+++ Git/practice.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+void welcome() {
+    printf(" Git practice.\n");
+}
+
+
+int factorial(int num) {
+    if (num < 0) return -1;
+    int result = 1;
+    for (int i = 1; i <= num; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int input;
+    welcome();
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &input);
+
+    int fact = factorial(input);
+    if (fact == -1) {
+        printf("Invalid input. Please enter a non-negative number.\n");
+    } else {
+        printf("Factorial of %d is %d\n", input, fact);
+    }
+
+    printf("Program ended.\n");
+    return 0;
+}
