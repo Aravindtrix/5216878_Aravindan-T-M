da3ce89379e7dc77f04e5acc02f30f92a0724d18
AravindTrix <aravindtrix@gmail.com>
2025-08-10 12:12:06 +0530

    added Hackerrank solution

---
+++ HackerRank/Week 1/Pangrams.c
@@ -0,0 +1,97 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+
+char* pangrams(char* s) {
+    bool seen[26] = {false};
+    int unique = 0;
+
+    for (char *p = s; *p; ++p) {
+        if (isalpha((unsigned char)*p)) {
+            int idx = tolower((unsigned char)*p) - 'a';
+            if (!seen[idx]) {
+                seen[idx] = true;
+                if (++unique == 26) break;   
+            }
+        }
+    }
+
+    return (unique == 26) ? "pangram" : "not pangram";
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = pangrams(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
